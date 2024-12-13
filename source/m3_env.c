c2870b159a0dba01888940ef31e21ddc909e2b31
Riccardo Cecchini <rcecchini.ds@gmail.com>
2024-12-14 00:44:08 +0100

    Update m3_env.c

--- source/m3_env.c
+++ source/m3_env.c
@@ -234,9 +234,12 @@ IM3Runtime  m3_NewRuntime  (IM3Environment i_environment, u32 i_stackSizeInBytes
 
         /// Preparing the stack is no more necessary      
 
+        size_t stackSize = i_stackSizeInBytes + 4 * sizeof (m3slot_t);
+        if(WASM_DEBUG_NEW_RUNTIME) ESP_LOGI("WASM3", "m3_NewRuntime: allocating originStack (%d)", stackSize);
+
         //runtime->originStack = m3_Malloc (memory, i_stackSizeInBytes + 4 * sizeof (m3slot_t)); // TODO: more precise stack checks
-        //runtime->originStack = m3_NewStack(); // (not implemented) ad hoc M3Memory for stack
-        runtime->originStack = m3_Def_Malloc (i_stackSizeInBytes + 4 * sizeof (m3slot_t)); // default malloc
+        //runtime->originStack = m3_NewStack(); // (not implemented) ad hoc M3Memory for stack        
+        runtime->originStack = m3_Def_Malloc (stackSize); // default malloc
 
         if (runtime->originStack)
         {
