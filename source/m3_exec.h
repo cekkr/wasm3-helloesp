9579786cf3833fcf494f816f92b80a5068162be0
Riccardo Cecchini <rcecchini.ds@gmail.com>
2024-12-14 00:40:55 +0100

    normal origin stack

--- source/m3_exec.h
+++ source/m3_exec.h
@@ -1378,6 +1378,11 @@ d_m3Op  (ContinueLoopIf)
 d_m3Op (Const32) {
     u32 value = MEMACCESS(u32, _mem, _pc++);
     void* dest = (void*)m3SegmentedMemAccess(_mem, _sp + immediate(i32), sizeof(u32));
+    
+    if (!dest) {
+        return m3Err_mallocFailed;  // o un altro codice di errore appropriato
+    }
+
     * (u32*) dest = value;
     nextOp();
 }
@@ -1387,7 +1392,12 @@ d_m3Op (Const64) {
     u64 value = MEMACCESS(u64, _mem, _pc);  
     _pc += 2;  // Su ESP32 sempre 2 perché M3_SIZEOF_PTR == 4
 
-    void* dest = (void*)m3SegmentedMemAccess(_mem, _sp + immediate(i32), sizeof(u64));
+    void* dest = (void*)m3SegmentedMemAccess(_mem, _sp + immediate(i32), sizeof(u32));
+    
+    if (!dest) {
+        return m3Err_mallocFailed;  // o un altro codice di errore appropriato
+    }
+    
     memcpy(dest, &value, sizeof(u64));  
 
     nextOp();
