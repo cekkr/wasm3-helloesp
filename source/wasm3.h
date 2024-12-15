edbfd08cb1bbb4ece9b52d7b1b29414df6907094
Riccardo Cecchini <rcecchini.ds@gmail.com>
2024-12-15 13:24:03 +0100

    wdt

--- source/wasm3.h
+++ source/wasm3.h
@@ -432,7 +432,7 @@ d_m3ErrorConst  (trapStackOverflow,             "[trap] stack overflow")
 /// WATCHDOG
 ///
 
-#define ENABLE_WDT 0
+#define ENABLE_WDT 1
 
 #if ENABLE_WDT
 #define CALL_WATCHDOG esp_task_wdt_reset(); 
