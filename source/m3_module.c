157920ec727d2ac5466149cac57b46486fd548b9
Riccardo Cecchini <rcecchini.ds@gmail.com>
2024-12-23 14:13:06 +0100

    Update m3_module.c

--- source/m3_module.c
+++ source/m3_module.c
@@ -87,7 +87,10 @@ _try {
         }
 
         //io_module->functions = m3_Dyn_ReallocArray (&io_module->runtime->memory, io_module->functions, M3Function, i_totalFunctions);
-        ESP_LOGI("WASM", "PreallocFunctions: reallocating to module->functions %d bytes", i_totalFunctions*sizeof(M3Function));
+        if(WASM_DEBUG_PREALLOCFUNCTIONS){
+            ESP_LOGI("WASM", "PreallocFunctions: reallocating to module->functions %d bytes", i_totalFunctions*sizeof(M3Function));
+        }
+
         io_module->functions = m3_Def_ReallocArray (M3Function, io_module->functions, i_totalFunctions);
         io_module->allFunctions = i_totalFunctions;
 
